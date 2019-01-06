e2992ec18739b7455ffc150f24b3b866f8ef81fc
Persio Martinez <ingmartinez21@gmail.com>
2019-01-06 09:48:17 -0400

    Actualizado

--- Fundamentos-SQL/5-Ejercicios.sql
+++
@@ -1,4 +0,0 @@
---PUNTO 1: Crear un producto nuevo, ingresando su categoría y suplidor
---PUNTO 2: Identificar todos los suplidores que tienen la región en blanco y rellenar con el valor “N/A”
---PUNTO 3: Eliminar de la tabla de detalle de ordenes todos aquellos productos con valor 1
---PUNTO 4: Listar todos los registros de la tabla de territorios
